772f66c79f000ea598d48214bb4660303d986d30
winkooui <49779423+winkooui@users.noreply.github.com>
2022-03-16 14:32:39 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '3a9f345d179e4407acf6f8dc5765ad0b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '655‘s Websit’',
+  domain: 'https://www.notion.so/655-S-websit-3a9f345d179e4407acf6f8dc5765ad0b',
+  author: 'zzy',
 
   // open graph metadata (optional)
   description: 'Example site description',
